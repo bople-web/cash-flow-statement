e11548d28feabc7ad31a0325f69f972209d1f4c8
Márton Plell <bopleweb@gmail.com>
2024-07-28 22:29:08 +0200

    FIX: 'Income' is a type and must be imported using a type-only import when 'verbatimModuleSyntax' is enabled.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Income, useStatementStore, type Expense } from './stores/statement'
+import { type Income, useStatementStore, type Expense } from './stores/statement'
 
 const statementStore = useStatementStore()
 
